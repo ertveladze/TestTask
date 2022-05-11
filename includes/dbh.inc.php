ad5961f3f9f8cea8d10342e1f0f1e70a7904c65a
Elene Rtveladze <ertveladze9@gmail.com>
2022-05-11 20:58:15 +0400

    php

---
+++ includes/dbh.inc.php
@@ -0,0 +1,13 @@
+<?php
+
+class Dbh {
+    private $servername;
+    private $firstname;
+    private $lastname;
+    private $email;
+    private $dbname;
+
+    protected function connect (){
+        
+    }
+}
